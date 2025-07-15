f12a748aa672d0fb2889976b79e04677f2ec2aeb
imkunet <im@kunet.dev>
2025-07-15 00:08:33 -0400

    fix: nullable webhook parameter

--- core/src/main/kotlin/me/fireballs/brady/core/Webhooks.kt
+++ core/src/main/kotlin/me/fireballs/brady/core/Webhooks.kt
@@ -3,7 +3,8 @@ package me.fireballs.brady.core
 import club.minnced.discord.webhook.WebhookClient
 import club.minnced.discord.webhook.WebhookClientBuilder
 
-fun createOrNull(url: String): WebhookClient? {
+fun createOrNull(url: String?): WebhookClient? {
+    url ?: return null
     if (!WebhookClientBuilder.WEBHOOK_PATTERN.matcher(url).matches()) return null
     return WebhookClientBuilder(url).build()
 }
