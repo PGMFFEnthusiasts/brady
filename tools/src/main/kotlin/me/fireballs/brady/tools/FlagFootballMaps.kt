68b018905232dc138dd91fdcddde650ce5e9d8a2
imkunet <im@kunet.dev>
2025-07-29 15:49:21 -0400

    fix: ffmaps good

--- tools/src/main/kotlin/me/fireballs/brady/tools/FlagFootballMaps.kt
+++ tools/src/main/kotlin/me/fireballs/brady/tools/FlagFootballMaps.kt
@@ -14,17 +14,15 @@ class FlagFootballMaps : KoinComponent {
     private val pgm by inject<PGM>()
     private val mapOrder by inject<MapOrder>()
 
-    private val ffMaps = lazy {
-        pgm.mapLibrary.maps.asSequence()
-            .filter { it.name.contains("Brady") || it.name.contains("Bowl") }
-            .sortedBy { it.name }
-            .toList()
-    }
+    private fun ffMaps() = pgm.mapLibrary.maps.asSequence()
+        .filter { it.name.contains("Brady") || it.name.contains("Bowl") }
+        .sortedBy { it.name }
+        .toList()
 
-    private val menu = menubox(9 * (ffMaps.value.size / 9).coerceAtLeast(1)) {
+    private val menu = menubox(9 * 3) {
         title = "Flag Football Maps"
         cancelClicks()
-        ffMaps.value.forEachIndexed { index, map ->
+        ffMaps().forEachIndexed { index, map ->
             val item = itembox(if (map.name.contains("Bowl")) Material.BOWL else Material.PAPER)
                 .name(map.getStyledName(MapNameStyle.COLOR))
                 .loreComponentLines(map.authors.map { it.name.forWhom(player) })
@@ -45,14 +43,14 @@ class FlagFootballMaps : KoinComponent {
             tabCompleter {
                 StringUtils.complete(
                     StringUtils.textToSuggestion(subArgs.joinToString(" ")),
-                    ffMaps.value.map { StringUtils.textToSuggestion(it.name) }
+                    ffMaps().map { StringUtils.textToSuggestion(it.name) }
                 )
             }
 
             executor {
                 if (subArgs.isNotEmpty()) {
                     val query = StringUtils.suggestionToText(subArgs.joinToString(" "))
-                    val map = StringUtils.bestFuzzyMatch(query, ffMaps.value) { it.name }
+                    val map = StringUtils.bestFuzzyMatch(query, ffMaps()) { it.name }
                     if (map == null) err("Map not found!")
                     mapOrder.nextMap = map
                     pgm.matchManager
